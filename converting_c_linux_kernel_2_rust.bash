033a231df18df95bf8df3ba3162752071d2f2a06
aamazie <35693948+aamazie@users.noreply.github.com>
2024-09-02 22:37:11 -0400

    Update converting_c_linux_kernel_2_rust.bash

--- converting_c_linux_kernel_2_rust.bash
+++ converting_c_linux_kernel_2_rust.bash
@@ -1,5 +1,6 @@
 # Clone the Linux kernel repository
-#oldconfig
+# making with oldconfig, but you can do defconfig as well for a new configuration. 
+# defconfig will generate a default configuration file. can specify a particular target, e.g., x86_64_defconfig for x86_64 architecture.
 
 git clone --depth=1 https://github.com/Rust-for-Linux/linux.git maze-kernel
 cd maze-kernel
@@ -14,15 +15,15 @@ export LDFLAGS="-L/usr/local/lib"
 export CPPFLAGS="-I/usr/local/include"
 
 # Configure the kernel to enable Rust support, may need dependencies like flex, bison, etc, depending on your server environment.
-#There will be a ton of specs to know, so if you want to know your server processor, you can run:
-#lscpu
+# There will be a ton of specs to know, so if you want to know your server processor, you can run:
+# lscpu
 
 make oldconfig
-#or, to automatically set all options to default, run
+# or, to automatically set all options to default, run
 yes "" | make oldconfig
 
 # Otherwise, configure kernel as you see fit and answer prompts.
-#Knowing what your configuration needs are will greatly lessen your burden when installing dependencies.
+# Knowing what your configuration needs are will greatly lessen your burden when installing dependencies.
 
 # Compile the kernel with Rust components
 make -j$(nproc) 

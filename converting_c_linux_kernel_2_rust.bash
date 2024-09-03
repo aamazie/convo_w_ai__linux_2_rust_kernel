6afc4d741927c949656f54204633e93129942e42
aamazie <35693948+aamazie@users.noreply.github.com>
2024-09-02 21:50:02 -0400

    Update converting_c_linux_kernel_2_rust.bash

--- converting_c_linux_kernel_2_rust.bash
+++ converting_c_linux_kernel_2_rust.bash
@@ -1,4 +1,6 @@
 # Clone the Linux kernel repository
+#oldconfig
+
 git clone --depth=1 https://github.com/Rust-for-Linux/linux.git maze-kernel
 cd maze-kernel
 
@@ -8,8 +10,8 @@ export CARGO=cargo
 export CC=clang
 export HOSTCC=clang
 
-# Configure the kernel to enable Rust support
-make menuconfig
+# Configure the kernel to enable Rust support, may need dependencies like flex, bison, etc, depending on your server environment
+make oldconfig
 # Navigate to 'Kernel hacking' -> 'Rust support'
 
 # Compile the kernel with Rust components

24847c06f75c0c989ad5c9b0d24e42d91aae88aa
aamazie <35693948+aamazie@users.noreply.github.com>
2024-09-02 21:51:51 -0400

    Update converting_c_linux_kernel_2_rust.bash

--- converting_c_linux_kernel_2_rust.bash
+++ converting_c_linux_kernel_2_rust.bash
@@ -7,8 +7,11 @@ cd maze-kernel
 # Set up Rust environment
 export RUSTC=rustc
 export CARGO=cargo
-export CC=clang
+export CC=gcc
 export HOSTCC=clang
+export CFLAGS="-O2"
+export LDFLAGS="-L/usr/local/lib"
+export CPPFLAGS="-I/usr/local/include"
 
 # Configure the kernel to enable Rust support, may need dependencies like flex, bison, etc, depending on your server environment
 make oldconfig
